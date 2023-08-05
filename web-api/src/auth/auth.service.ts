b2fc05c2af572e7711f3af30eef6f7e06109e612
paderborner2019 <paderborner2019@gmail.com>
2023-08-05 18:01:51 +0300

    implement jwt strategy

--- web-api/src/auth/auth.service.ts
+++ web-api/src/auth/auth.service.ts
@@ -1,13 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config/dist/config.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async login(dto: AuthDto) {
-    return { ...dto, test: 'test' };
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Incorrect password');
+    }
+    return this.signToken(user.id, user.email);
   }
 
   async signUp(dto: AuthDto) {
-    return dto;
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+        throw error;
+      }
+    }
+  }
+
+  private async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ accessToken: string }> {
+    const payload = { sub: userId, email };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: secret,
+    });
+    return { accessToken: token };
   }
 }
--- web-api/src/auth/dto/auth.dto.ts
+++ web-api/src/auth/dto/auth.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Role } from '@prisma/client';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsEmail()
@@ -8,4 +9,7 @@ export class AuthDto {
   @IsString()
   @IsNotEmpty()
   password: string;
+
+  @IsOptional()
+  role?: Role;
 }
